7876e984a4d7251d3ea0618655617c49d610c883
Martijn Verbove <martijn@verbove.com>
2019-09-05 20:32:03 +0300

    Adds rental list component, adds rental detail component, adds rental service and rental model

---
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { RentalService } from '../shared/rental.service';
+import { Rental } from '../shared/rental.model';
+
+@Component({
+  selector: 'bwm-rental-list',
+  templateUrl: './rental-list.component.html',
+  styleUrls: ['./rental-list.component.scss']
+})
+export class RentalListComponent implements OnInit {
+
+  constructor(private rentalService: RentalService) { }
+
+  rentals: Rental[] =  [];
+
+  ngOnInit() {
+
+    const rentalObservable = this.rentalService.getRentals();
+
+    rentalObservable.subscribe(
+      (rentals: Rental[]) => {
+        this.rentals = rentals;
+      },
+      (err) => {
+      },
+      () => {
+      }
+    );
+  }
+
+}
